8a2b6783da297330fca9f15e6fc27ead2eb98975
azarel <azarelfuntes5@gmail.com>
2025-03-24 14:44:58 -0600

    act 24/02/2025

---
+++ vistas/header.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head class="encabezado">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <link rel="preload" href="css/normalize.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="preload" href="css/style.css" as="style">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <title>ATLACOMULCO</title>
+</head>
+<body onload="starTime()">
+    <header class="top">
+        <nav class="top-order">
+            <div class="clockdate">
+                <span id="time"></span>
+                <span id="date"></span>
+            </div>
+            <!-- <div class="wraper-atlacomulco">
+                <div class="dinamic-atlacomulco">
+                    Atlacomulco
+                </div>
+                    <ul class="dinamic-text">
+                        <li><span>tierra de trabajadores</span></li>
+                        <li><span>hermoso</span></li>
+                        <li><span>maravila </span></li>
+                        <li><span>atlacomulco</span></li>
+                    </ul>
+            </div>
+            <p class="fecha">vienes 14 de febrero </p> -->
+            <div class="slider">
+                <div class="slide-track">
+                    <div class="slide">
+                        <P>atlacomulco</P>
+                    </div>
+                    <div class="slide">
+                        <p>san juan de los jarros</p>
+                    </div>
+                    <div class="slide">
+                        <p>san lorenzo tlacotepec</p>
+                    </div>
+                    <div class="slide">
+                        <p>santiago acutlizapan</p>
+                    </div>
+                    <div class="slide">
+                        <p>san jeronimo de la jarros</p>
+                    </div>
+                    <div class="slide">
+                        <p>bobashi de guadalupe</p>
+                    </div>
+                    <div class="slide">
+                        <p>la mesa de chosto</p>
+                    </div>
+                    <div class="slide">
+                        <p>bombatevi ejido</p>
+                    </div>
+                    <div class="slide">
+                        <p>bongoni</p>
+                    </div>
+                    <div class="slide">
+                        <p>san luis boro</p>
+                    </div>
+                    <div class="slide">
+                        <p>las tarrias</p>
+                    </div>
+                    <div class="slide">
+                        <p>san jose del tunal</p>
+                    </div>
+                    <div class="slide">
+                        <p>dolores la joya</p>
+                    </div>
+                    <div class="slide">
+                        <p>san ignacion de loyola</p>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+        <nav class="nav-container">
+            <div class="nav-logo">
+                <a href="#">
+                    <img src="img/toponimio-185x291.png" alt="">
+                </a>
+            </div>
+            <i class="fa-solid fa-bars open-menu"></i>
+            <div class="nav-link-container">
+                <i class="fa-solid fa-xmark close-menu"></i>
+                <ul class="nav-links">
+                    <li class="nav-link-items"><a href="#">normatividad</a></li>
+                    <li class="nav-link-items dropdown-menu-branch"><a href="#" data-toggle="dropdown-menu">transparencia  <i class="fa-solid fa-chevron-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li class="dropdown-menu-item"><a href="#">aviso de privacidad</a></li>
+                            <li class="dropdown-menu-item"><a href="#">has tu solicitud</a></li>
+                            <li class="dropdown-menu-item"><a href="#">ipomex</a></li>
+                            <li class="dropdown-menu-item"><a href="#">tranparencia proactiva</a></li>
+                            <li class="dropdown-menu-item"><a href="#">lgcg</a></li>
+                            <li class="dropdown-menu-item"><a href="#">apertura de las unidades economicas</a></li>
+                            <li class="dropdown-menu-item"><a href="#">bitacora ambiental</a></li>
+                            <li class="dropdown-menu-item"><a href="#">archivo municipal</a></li>
+                            <li class="dropdown-menu-item"><a href="#">uippe</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-link-items dropdown-menu-branch"><a href="#" data-toggle="dropdown-menu">mejora regulatoria <i class="fa-solid fa-chevron-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li class="dropdown-menu-item"><a href="#">sare</a></li>
+                            <li class="dropdown-menu-item"><a href="#">comisión municipal de mejora regulatoria</a></li>
+                            <li class="dropdown-menu-item"><a href="#">retys/remty's(municipal)</a></li>
+                            <li class="dropdown-menu-item"><a href="#">catalogos</a></li>
+                            <li class="dropdown-menu-item"><a href="#">consulta pública</a></li>
+                            <li class="dropdown-menu-item"><a href="#">protesta ciudadana</a></li>
+                            <li class="dropdown-menu-item"><a href="#">agenda regulatoria</a></li>
+                            <li class="dropdown-menu-item"><a href="#">politica municipal de mejora regulatoria</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-link-items dropdown-menu-branch"><a href="#" data-toggle="dropdown-menu">sitio web <i class="fa-solid fa-chevron-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li class="dropdown-menu-item"><a href="#">correo institucional</a></li>
+                            <li class="dropdown-menu-item"><a href="#">levantar tiket(star)</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-link-items"><a href="#">atencion ciudadana</a></li>
+                </ul>  
+            </div>
+        </nav>
